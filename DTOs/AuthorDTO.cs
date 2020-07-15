8ca63804beb13006861af997c6fe47252673f5fd
Michael Allen Smith <digitalcolony@gmail.com>
2020-07-15 11:33:53 -0700

    thru 16

---
+++ DTOs/AuthorDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore_API.DTOs
+{
+    public class AuthorDTO
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Bio { get; set; }
+
+        public virtual IList<BookDTO> Books { get; set; }
+    }
+}
