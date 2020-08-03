829e7f1a84f88ae66d257044b268f7c42737bae9
Michael Allen Smith <digitalcolony@gmail.com>
2020-08-03 07:21:23 -0700

    Blazor DTO

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -34,12 +34,43 @@ namespace BookStore_API.Controllers
             _config = config;
         }
 
+        [Route("register")]
+        [HttpPost]
+        public async Task<IActionResult> Register([FromBody] UserDTO userDTO)
+        {
+            var location = GetControllerActionNames();
+
+            try
+            {
+                var username = userDTO.EmailAddress;
+                var password = userDTO.Password;
+                _logger.LogInfo($"{location}: Registration Attempt for {username} ");
+                var user = new IdentityUser { Email = username, UserName = username };
+                var result = await _userManager.CreateAsync(user, password);
+
+                if(!result.Succeeded)
+                {
+                    foreach(var error in result.Errors)
+                    {
+                        _logger.LogInfo($"{location}: {error.Code} {error.Description}");
+                    }
+                    return InternalError($"{location}: {username} User Registration Failed");
+                }
+
+                return Ok(new { result.Succeeded });
+            }
+            catch (Exception e)
+            {
+                return InternalError($"{location}: {e.Message} - {e.InnerException}");
+            }
+        }
         /// <summary>
         /// User Login Endpoint
         /// </summary>
         /// <param name=""></param>
         /// <param name="userDTO"></param>
         /// <returns></returns>
+        [Route("login")]
         [AllowAnonymous]
         [HttpPost]
         public async Task<IActionResult> Login([FromBody] UserDTO userDTO)
@@ -48,7 +79,7 @@ namespace BookStore_API.Controllers
 
             try
             {
-                var username = userDTO.Username;
+                var username = userDTO.EmailAddress;
                 var password = userDTO.Password;
 
                 _logger.LogInfo($"{location}: Login Attempt from user {username} ");
--- DTOs/UserDTO.cs
+++ DTOs/UserDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +8,13 @@ namespace BookStore_API.DTOs
 {
     public class UserDTO
     {
-        public string Username { get; set; }
-
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress]
+        public string EmailAddress { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(50, ErrorMessage = "Your Password is limited to {2} to {1} characters", MinimumLength = 6)]
         public string Password { get; set; }
 
     }
